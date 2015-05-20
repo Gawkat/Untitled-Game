12ca1d26f1aed748dfd158bbc6737629ebbf2239
Gawkat <hp_johannes@hotmail.com>
2015-05-20 18:58:55 +0200

    First commit

---
+++ desktop/src/com/github/gawkat/untitledgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.gawkat.untitledgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.gawkat.untitledgame.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
